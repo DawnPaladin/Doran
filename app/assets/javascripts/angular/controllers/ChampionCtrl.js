f675ee7a3fe8a576ab28f1c02decbcf16d246b89
James Harris <computersherpa@gmail.com>
2017-06-11 18:47:22 -0500

    Filterable champion cards

--- app/assets/javascripts/angular/controllers/ChampionCtrl.js
+++ app/assets/javascripts/angular/controllers/ChampionCtrl.js
@@ -1,5 +1,6 @@
 Doran.controller('ChampionCtrl', ['$scope', 'championService', function($scope, championService) {
   championService.getAll().then(function(champions) {
     $scope.champions = champions;
+    console.log(champions);
   });
 }]);
--- app/assets/javascripts/angular/services/championService.js
+++ app/assets/javascripts/angular/services/championService.js
@@ -1,12 +1,6 @@
 Doran.factory('championService', ['Restangular', function(Restangular) {
   var exports = {};
 
-  // var champions = Restangular.all('champions');
-  // champions.getList().then(function(champions) {
-  //   exports.champions = champions;
-  //   console.log(champions);
-  // });
-
   exports.getAll = function() {
     return Restangular.all('champions').getList();
   };
